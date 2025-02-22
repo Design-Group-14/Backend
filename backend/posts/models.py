970bc187cc3aab3e4fedff26db6ff11900782b85
Tanuj Sood <soodt@tcd.ie>
2025-02-22 17:37:56 +0000

    fixing attribute error of mismatched field names

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.URLField(max_length=500, blank =True, null=True)
+    image_url = models.URLField(max_length=500, blank =True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
