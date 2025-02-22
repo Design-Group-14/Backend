945f6b073a8bca5f4e64b77610544a768c6c5a87
Tanuj Sood <soodt@tcd.ie>
2025-02-22 17:11:36 +0000

    setting up posts functionality with views and URLs, updating post type to take image urs

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,14 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    image = models.URLField(max_length=500, blank =True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
-        return self.title
+        return f"Post {self.title} by {self.user.email} at {self.created_at}"
 
