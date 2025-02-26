64eb2388bfe6832fd4b35eed52234bf09826f1bc
Steve Thomas <steve13thomas@gmail.com>
2025-02-26 17:44:54 +0000

    migrated from default user model to custom user model

--- backend/tcd_social/settings.py
+++ backend/tcd_social/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'django_extensions',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+AUTH_USER_MODEL = 'users.User'
 
 
 # Internationalization
