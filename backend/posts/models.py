0ff48489b3da06334d8972261de7edd5f8126535
Tanuj Sood <soodt@tcd.ie>
2025-03-19 21:16:48 +0000

    Implemented locations

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -3,11 +3,13 @@ from django.conf import settings
 
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, default="Untitled Post")
     content = models.TextField()
     image_url = models.URLField(max_length=500, blank =True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
 
     class Meta:
         ordering = ['-created_at']
